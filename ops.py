cdd5a4d39a1fc9dc0da09b472c3fbe2a3af3de31
faisal-hajari <faisalhejary@gmail.com>
2024-11-14 10:59:33 +0300

    feat: create functions for better query

---
+++ ops.py
@@ -0,0 +1,74 @@
+from llm import LLM, Prompt, VectorDB
+import prompts
+
+
+def gen_history(session_messages:list[dict[str:str]]) -> str:
+    history = ""
+    assistant= Prompt(prompts.role_assistant_prompt)
+    user = Prompt(prompts.role_user_prompt)
+
+    for message in session_messages:
+        if message["role"] == "assistant":
+            history+= "\n"+assistant(response=message["content"])
+        elif message["role"] == "user":
+            history+= "\n"+user(query=message["content"])
+        else:
+            raise ValueError("Unknown role in message")
+    return history
+
+
+def query_rewriter(llm:LLM, session_messages:list[dict[str:str]]) -> str:
+    prompt = Prompt(prompts.chat_prompt)(
+            system_prompt=prompts.query_rewriting_prompt,
+            history=gen_history(session_messages),
+            assistant_prompt= Prompt(prompts.role_assistant_prompt)(response="")
+    )
+    response = llm(prompt)
+    response = llm("extract the exact query from the text do not add any other text: {response}".format(response=response))
+    return response, prompt
+
+
+def multi_query(llm:LLM, query:str) -> str:
+    prompt = Prompt(prompts.chat_prompt)(
+            system_prompt=prompts.multi_query_prompt,
+            history=Prompt(prompts.role_user_prompt)(query=query),
+            assistant_prompt= Prompt(prompts.role_assistant_prompt)(response="")
+    )
+    response = llm(prompt)
+    response = llm("extract the exact five query from the text do not add any other text: {response}".format(response=response))
+    return response, prompt
+
+
+def concat_docs(docs):
+    context = "".join([doc.page_content+"\n\n" for doc in docs])
+    return context
+
+def referance_gen(doc):
+    source = doc.metadata["source"].split("/")[-1]
+    page = doc.metadata["page"]
+    referance = f"{source} : {page}"
+    return referance
+
+def concat_docs_with_referance(docs):
+    context = "\n".join(
+        [f"{referance_gen(doc)}\n{doc.page_content}\n\n" 
+         for doc in docs ]
+    )
+    return context
+
+def rag_with_ref(llm:LLM, vdb:VectorDB, messages:list[dict[str:str]]) -> str:
+    rew_query, _ = query_rewriter(llm, messages)
+    multi_query_res, _ = multi_query(llm, rew_query)
+    queries = multi_query_res.split("\n")
+    docs = [] 
+    for query in queries:
+        docs.extend(vdb.search(query)) 
+    context = concat_docs_with_referance(docs)
+
+    rag_prompt = Prompt(prompts.chat_prompt)(
+            system_prompt=Prompt(prompts.rag_systme_prompt)(context=context),
+            history=gen_history(messages),
+            assistant_prompt= Prompt(prompts.role_assistant_prompt)(response="")
+    )
+    response = llm(rag_prompt)
+    return response, rag_prompt
--- prompts.py
+++ prompts.py
@@ -1,15 +1,48 @@
-retrieval_prompt = """\
-Question: {question}
+rag_systme_prompt = """You are an advanced AI language model assistant for question-answering tasks specialized in AI eithics. 
+Use the following pieces of retrieved context to answer the question. If you
+don't know the answer, just say that you don't know. Use three sentences
+maximum and keep the answer concise.
+context:
+{context}"""
 
-You are an intelligent assistant. Your task is to answer the question using \
-only the provided context. Do not use any outside knowledge or make \
-assumptions. If the answer is not in the context, simply state that the \
-information is not available.
+multi_query_prompt = """You are an AI language model assistant. Your task is to generate five
+different versions of the given user question to retrieve relevant documents from a vector 
+database. By generating multiple perspectives on the user question, your goal is to help
+the user overcome some of the limitations of the distance-based similarity search. 
+Provide these alternative questions separated by newlines.
+think about it step by step, and provide a detailed and well-thought-out explanation of the different perspectives.
+fromat your response as follows:
+reasoning:
+{your reasoning here}
+question:
+{your alternative question here}
+{your alternative question here}
+{your alternative question here}
+{your alternative question here}
+{your alternative question here}
+"""
+query_rewriting_prompt = """You are an advanced AI language model assistant. 
+Your task is to rephrase the user's question to enhance the relevance and quality of documents retrieved from a vector database.
+By refining the user's query, your goal is to mitigate the limitations of distance-based similarity search and ensure more accurate and contextually appropriate results.
+keep the question concise and clear, and try to capture the user's intent effectively.
+think about it step by step, and provide a detailed and well-thought-out revision of the user's question.
+write your though process and rationale after "reasoning: "
+wtire your revised question after "question: " 
+fromat your response as follows:
+reasoning:
+{your reasoning here}
+question:
+{your revised question here}
+"""
 
-Context:
-{context}
+role_user_prompt = """<|eot_id|><|start_header_id|>user<|end_header_id|>
+{query}"""
 
-Question: {question}
+role_assistant_prompt = """<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+{response}"""
 
-Answer: 
+chat_prompt ="""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+{system_prompt}
+{history}
+{assistant_prompt}
 """
